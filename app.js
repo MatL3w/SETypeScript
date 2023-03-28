45d78e4c76502f2f3507f69c75371935bcfbc3ae
Lecislav <lecislav@protonmail.com>
2023-03-28 13:38:07 +0200

    final

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 "use strict";
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById("num2");
+const buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 5));
-console.log(add('1', '5'));
+buttonElement.addEventListener('click', () => {
+    const num1 = +num1Element.value;
+    const num2 = +num1Element.value;
+    const result = add(num1, num2);
+    console.log(result);
+});
